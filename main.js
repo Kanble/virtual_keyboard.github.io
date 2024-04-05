a5d0c2a0b937ac29ef9496d64e30d34960b41b9d
Kanble <danila_koba@mail.ru>
2024-04-05 19:55:29 +0300

    add

--- main.js
+++ main.js
@@ -6,77 +6,132 @@ let caps_lock_key = document.querySelector('.caps_lock_key');
 let toggle_circle = document.querySelector('.toggle_circle');
 let night_mode = document.querySelector('.night_mode');
 let body = document.querySelector('body');
-let text_input = document.querySelector('.text');
+let textInput = document.querySelector('.text');
 let change_color = document.querySelector('.change_light_color');
 let colors_input = document.querySelector('.colors_input');
 let keyboard_lights = document.querySelector('.keyboard_lights');
 let keyboard_wrapp = document.querySelector('.keyboard_wrapp');
 
-for(let i = 0; i < keys.length; i++) {
-    keys[i].setAttribute('keyname', keys[i].innerText);
-    keys[i].setAttribute('lowerCaseName', keys[i].innerText.toLowerCase());
+for (let i = 0; i < keys.length; i++) {
+  keys[i].setAttribute('keyname', keys[i].innerText);
+  keys[i].setAttribute('lowerCaseName', keys[i].innerText.toLowerCase());
 }
 
 window.addEventListener('keydown', function(e) {
-    for(let i = 0; i < keys.length; i++) {
-        if(e.key == keys[i].getAttribute('keyname' ) || e.key == keys[i].getAttribute('lowerCaseName')) {
-            keys[i].classList.add('active')
-        }
-        if(e.code == 'Space') {
-            spaceKey.classList.add('active')
-        }
-        if(e.code == 'ShiftLeft') {
-            shift_right.classList.remove('active')
-        }
-        if(e.code == 'ShiftRight') {
-            shift_left.classList.remove('active')
-        }
-        if(e.code == 'CapsLock') {
-            caps_lock_key.classList.toggle('active');
-        }
+    if (e.target !== textInput && e.key.length === 1) {
+      e.preventDefault();
+      textInput.focus();
+      textInput.value += e.key;
     }
-})
+  
+    if (e.code === 'Space') {
+      e.preventDefault();
+      textInput.focus();
+      textInput.value += ' ';
+    }
+  
+    for (let i = 0; i < keys.length; i++) {
+      if (
+        (e.key == keys[i].getAttribute('keyname') ||
+          e.key == keys[i].getAttribute('lowerCaseName')) &&
+        e.key.length === 1
+      ) {
+        keys[i].classList.add('active');
+      }
+  
+      if (e.code == 'ShiftLeft') {
+        shift_right.classList.remove('active');
+      }
+      if (e.code == 'ShiftRight') {
+        shift_left.classList.remove('active');
+      }
+      if (e.code == 'CapsLock') {
+        caps_lock_key.classList.toggle('active');
+      }
+    }
+  });
 
 window.addEventListener('keyup', function(e) {
-    for(let i = 0; i < keys.length; i++) {
-        if(e.key == keys[i].getAttribute('keyname' ) || e.key == keys[i].getAttribute('lowerCaseName')) {
-            keys[i].classList.remove('active')
-            keys[i].classList.add('remove')
-        }
-        if(e.code == 'Space') {
-            spaceKey.classList.remove('active');
-            spaceKey.classList.add('remove');
-        }
-        if(e.code == 'ShiftLeft') {
-            shift_right.classList.remove('active')
-            shift_right.classList.remove('remove')
-        }
-        if(e.code == 'ShiftRight') {
-            shift_left.classList.remove('active')
-            shift_left.classList.remove('remove')
-        }
-        setTimeout(()=> {
-            keys[i].classList.remove('remove')
-        },200)
+  for (let i = 0; i < keys.length; i++) {
+    if (
+      (e.key == keys[i].getAttribute('keyname') ||
+        e.key == keys[i].getAttribute('lowerCaseName')) &&
+      e.key.length === 1
+    ) {
+      keys[i].classList.remove('active');
+      keys[i].classList.add('remove');
     }
-})
-
-
-night_mode.addEventListener('click',function() {
-    toggle_circle.classList.toggle('active');
-    body.classList.toggle('active');
-    night_mode.classList.toggle('active');
-    keyboard_wrapp.classList.toggle('active');
-    text_input.classList.toggle('active');
-    change_color.classList.toggle('active');
-    for(let i = 0; i < keys.length; i++) {
-        keys[i].classList.toggle('keys_night')
+    if (e.code == 'Space') {
+      spaceKey.classList.remove('active');
+      spaceKey.classList.add('remove');
     }
-})
-
-colors_input.addEventListener('input',function() {
-    for(let i = 0; i < keys.length; i++) {
-        keys[i].style.color = colors_input.value
+    if (e.code == 'ShiftLeft') {
+      shift_right.classList.remove('active');
+      shift_right.classList.remove('remove');
     }
-    keyboard_lights.style.background = colors_input.value;
-})
+    if (e.code == 'ShiftRight') {
+      shift_left.classList.remove('active');
+      shift_left.classList.remove('remove');
+    }
+    setTimeout(() => {
+      keys[i].classList.remove('remove');
+    }, 200);
+  }
+});
+
+for (let i = 0; i < keys.length; i++) {
+  keys[i].addEventListener('click', function() {
+    keys[i].classList.add('active');
+    setTimeout(() => {
+      keys[i].classList.remove('active');
+      keys[i].classList.add('remove');
+    }, 200);
+  });
+}
+
+spaceKey.addEventListener('click', function() {
+  spaceKey.classList.add('active');
+  setTimeout(() => {
+    spaceKey.classList.remove('active');
+    spaceKey.classList.add('remove');
+  }, 200);
+});
+
+shift_left.addEventListener('click', function() {
+  shift_left.classList.add('active');
+  setTimeout(() => {
+    shift_left.classList.remove('active');
+    shift_left.classList.remove('remove');
+  }, 200);
+});
+
+shift_right.addEventListener('click', function() {
+  shift_right.classList.add('active');
+  setTimeout(() => {
+    shift_right.classList.remove('active');
+    shift_right.classList.remove('remove');
+  }, 200);
+});
+
+caps_lock_key.addEventListener('click', function() {
+  caps_lock_key.classList.toggle('active');
+});
+
+night_mode.addEventListener('click', function() {
+  toggle_circle.classList.toggle('active');
+  body.classList.toggle('active');
+  night_mode.classList.toggle('active');
+  keyboard_wrapp.classList.toggle('active');
+  text_input.classList.toggle('active');
+  change_color.classList.toggle('active');
+  for (let i = 0; i < keys.length; i++) {
+    keys[i].classList.toggle('keys_night');
+  }
+});
+
+colors_input.addEventListener('input', function() {
+  for (let i = 0; i < keys.length; i++) {
+    keys[i].style.color = colors_input.value;
+  }
+  keyboard_lights.style.background = colors_input.value;
+});
